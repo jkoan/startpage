6dfb0ebd6c4017c9bcd895cfe6e0f43e078494aa
Jkoan <jkoan@gmx.de>
2014-06-21 09:29:31 +0200

    dev_2

--- js/tutorial.js
+++ js/tutorial.js
@@ -14,25 +14,77 @@ function getCookie(cname) {
     }
     return "";
 }
+function check_settings() {
+  	var json = getCookie('index')
+  	settings = JSON.parse(json)
+  	
+}
 
-function checkCookie() {
+function checkinit() {
     var init=getCookie("init");
     if (init == "") {
         $('#step_1').modal('show');
     }
     else{
-    	
+    	var index=getCookie("index");
+    	json_index = JSON.parse(index);
     }
 }
+function loadSecondScript(src) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    $("head").append(script);
+}
 function step_1() {
+	$('#step_1').modal({backdrop:false});
 	setCookie("init","ok",365);
-	$('#step_1').modal('hide');
-	$('#step_2').modal('show');
+	$('#step_2').modal({backdrop:false});
 }
 
 function step_2() {
-  
+	setCookie("init","ok",365);
 }
 function step_3() {
+	var json = {
+		"rss" : true,
+		"google" : true
+	}
+	var s_json = JSON.stringify(json)
+	setCookie("index",s_json,365);
+}
+
+function show_settings(){
+	if(settings.rss == true){
+		$('#rss_on').addClass('active')
+	}
+	else{
+		$('#rss_off').addClass('active')
+	}
+	if(settings.google == true){
+		$('#google_on').addClass('active')
+	}
+	else{
+		$('#google_off').addClass('active')
+	}
+
+	$('#settings').modal('show')
+}
+
+function save_settings() {
+  if($('#rss_on').hasClass('active')){
+  	settings.rss = true
+  }
+  if($('#rss_off').hasClass('active')){
+  	settings.rss = false
+  }
+  if($('#google_on').hasClass('active')){
+  	settings.rss = true
+  }
+  if($('#google_off').hasClass('active')){
+  	settings.rss = false
+  }
   
+  var s_json = JSON.stringify(settings)
+  setCookie("index",s_json,365);
 }
