6d9413b633ee1bcc2c57219147ff3cc3e605dda0
jkoan <jkoan@gmx.de>
2014-06-23 18:24:04 +0200

    More pretty Code

---
+++ js/core.js
@@ -0,0 +1,122 @@
+/**
+ * @author jkoan@jkoan.de
+ */
+
+function main() {
+    checkinit();
+    check_settings();
+    if (settings.rss == true){
+        rss(settings.rss_feed);
+    }
+    if(settings.google = true){
+        $("#search_google").attr("target","_blank");
+    }
+}
+
+function rss (feed) {
+    if (learnRegExp(feed)){
+    $('#rss').FeedEk({
+        FeedUrl: feed,
+        MaxCount: 10,
+        ShowDesc: true,
+        ShowPubDate: false,
+        DescCharacterLimit: 150
+        });
+    }
+}
+
+ function learnRegExp(s) {    
+      var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+      return regexp.test(s);    
+ }
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname+"="+cvalue+"; "+expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function check_settings() {
+    var json = getCookie('index');
+    if (json != "") {
+        settings = JSON.parse(json);
+    }
+}
+
+function checkinit() {
+    var init=getCookie("init");
+    if (init == "") {
+        load_js_script("js/tutorial.js");
+        $('#step_1').modal('show');
+    }
+    else{
+        var index=getCookie("index");
+        json_index = JSON.parse(index);
+    }
+}
+
+function load_js_script(src) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    $("head").append(script);
+}
+
+function show_settings(){
+    if(settings.rss == true){
+        $('#rss_on').addClass('active');
+        $('#rss_off').removeClass('active');
+    }
+    else{
+        $('#rss_off').addClass('active');
+        $('#rss_on').removeClass('active');
+        
+    }
+    if(settings.google == true){
+        $('#google_on').addClass('active');
+        $('#google_off').removeClass('active');
+    }
+    else{
+        $('#google_off').addClass('active');
+        $('#google_on').removeClass('active');
+    }
+    if(settings.rss_feed != ""){
+        $('#google_on').val(settings.rss_feed);
+    }
+    else{
+        $('#google_on').val();
+    }
+    $('#modal_settings').modal('show');
+}
+
+function save_settings() {
+    if($('#rss_on').hasClass('active')){
+        settings.rss = true;
+    }
+    if($('#rss_off').hasClass('active')){
+        settings.rss = false;
+    }
+    if($('#google_on').hasClass('active')){
+        settings.google = true;
+        $("#search_google").attr("target","_blank");
+
+    }
+    if($('#google_off').hasClass('active')){
+        settings.google = false;
+        $("#search_google").attr("target","");
+    }
+    var s_json = JSON.stringify(settings);
+    setCookie("index",s_json,365);
+    $('#modal_settings').modal('hide');
+}
--- js/tutorial.js
+++ js/tutorial.js
@@ -1,45 +1,11 @@
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname+"="+cvalue+"; "+expires;
-}
+/**
+ * @author jkoan@jkoan.de
+ */
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i].trim();
-        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-    }
-    return "";
-}
-function check_settings() {
-  	var json = getCookie('index')
-  	settings = JSON.parse(json)
-  	
-}
-
-function checkinit() {
-    var init=getCookie("init");
-    if (init == "") {
-        $('#step_1').modal('show');
-    }
-    else{
-    	var index=getCookie("index");
-    	json_index = JSON.parse(index);
-    }
-}
-function loadSecondScript(src) {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = src;
-    $("head").append(script);
-}
 function step_1() {
-	$('#step_1').modal({backdrop:false});
+	$('#step_1').modal('hide');
 	setCookie("init","ok",365);
-	$('#step_2').modal({backdrop:false});
+	$('#step_2').modal('show');
 }
 
 function step_2() {
@@ -49,42 +15,7 @@ function step_3() {
 	var json = {
 		"rss" : true,
 		"google" : true
-	}
-	var s_json = JSON.stringify(json)
+	};
+	var s_json = JSON.stringify(json);
 	setCookie("index",s_json,365);
-}
-
-function show_settings(){
-	if(settings.rss == true){
-		$('#rss_on').addClass('active')
-	}
-	else{
-		$('#rss_off').addClass('active')
-	}
-	if(settings.google == true){
-		$('#google_on').addClass('active')
-	}
-	else{
-		$('#google_off').addClass('active')
-	}
-
-	$('#settings').modal('show')
-}
-
-function save_settings() {
-  if($('#rss_on').hasClass('active')){
-  	settings.rss = true
-  }
-  if($('#rss_off').hasClass('active')){
-  	settings.rss = false
-  }
-  if($('#google_on').hasClass('active')){
-  	settings.rss = true
-  }
-  if($('#google_off').hasClass('active')){
-  	settings.rss = false
-  }
-  
-  var s_json = JSON.stringify(settings)
-  setCookie("index",s_json,365);
 }
